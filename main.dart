c20821df0cca8dc355a0d42517e090dbeee7ee6b
Gage Milton <richardson.milton@my.tccd.edu>
2019-12-20 20:53:35 +0000

    Initial commit

---
+++ main.dart
@@ -0,0 +1,17 @@
+// learning dart 
+
+int git = 100;
+
+
+ String fromName = "Frog";
+
+var num = 4; 
+
+
+
+void main() {
+	print('Hello World!');
+  print(git);
+  print(fromName);
+  print(num);
+}
